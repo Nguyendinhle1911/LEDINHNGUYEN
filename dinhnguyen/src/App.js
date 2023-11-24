aab70c5bd3842de75be8a5a1c75bb1ae6a9f014a
nguyen1911 <143236655+nguyen1911@users.noreply.github.com>
2023-11-24 17:45:14 +0700

    dfjfi0f

--- dinhnguyen/src/App.js
+++ dinhnguyen/src/App.js
@@ -1,19 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./style.css"
+import Product from "./product";
 import { useState } from "react";
-import React,(useState) from "react";
+import Input from "./input";
+import Button from "./button";
 
-function App(){
- const [headingText, setHeadingText]= useState("hello");
- function handleClick(){
-    headingText("sumitted");
- }
-
- return(
-    <div>
-        <h1>{headingText}</h1>
-        <button>
-            
-        </button>
+export default function App(){
+  return(
+    <div className="App">
+      <input
+      name="email"
+      type="text"
+      lang="Email"
+      onChange={handleEmailInput}
+      isValid={email.isValid}/>
     </div>
- );
+  );
 }
+
 export default App;
