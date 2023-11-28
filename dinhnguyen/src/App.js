14f357bb27cf3b5c1876d55a34a8833af3300474
nguyen1911 <143236655+nguyen1911@users.noreply.github.com>
2023-11-28 11:03:00 +0700

    baithi

--- dinhnguyen/src/App.js
+++ dinhnguyen/src/App.js
@@ -1,22 +1,80 @@
-import logo from "./logo.svg";
-import "./App.css";
-import "./style.css"
-import Product from "./product";
-import { useState } from "react";
-import Input from "./input";
-import Button from "./button";
+import React from "react";
 
-export default function App(){
-  return(
-    <div className="App">
-      <input
-      name="email"
-      type="text"
-      lang="Email"
-      onChange={handleEmailInput}
-      isValid={email.isValid}/>
+export default function Book() {
+  return (
+    <div>
+      <meta charSet="UTF-8" />
+      <title>AngularJS Practical Examples</title>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"
+      />
+      <div className="container">
+        <div className="input-group">
+          <span className="input-group-addon">
+            <span className="glyphicon glyphicon-search" />
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            ng-model="main.searchInput"
+          />
+        </div>
+        <h3>A list of Books</h3>
+        <ul className="list-group">
+          <li
+            className="list-group-item"
+            ng-repeat="show in main.shows | filter:main.searchInput | orderBy:main.order.key:main.order.reverse"
+          >
+            <span className="glyphicon glyphicon-star" ng-if="show.favorite" />{" "}
+            <span className="badge">
+              {"{"}
+              {"{"}show.author{"}"}
+              {"}"}
+            </span>
+          </li>
+        </ul>
+        <select
+          className="form-control pull-right"
+          ng-model="main.order"
+          ng-options="order as order.title for order in main.orders"
+        />
+        <div className="clearfix" />
+        <h3>Add a new Book</h3>
+        <form name="main.addForm" className="form" ng-submit="main.addShow()">
+          <div className="form-group">
+            <label>Title</label>
+            <input
+              type="text"
+              className="form-control"
+              ng-model="main.new.title"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label>Author</label>
+            <input
+              type="text"
+              className="form-control"
+              ng-model="main.new.author"
+              required
+            />
+            {/*<input type="number" min="1900" max="2030" class="form-control" ng-model="main.new.year" required />*/}
+          </div>
+          <div className="row">
+            <div className="col-xs-6">
+              <label>
+                Favorite: <input type="checkbox" ng-model="main.new.favorite" />
+              </label>
+            </div>
+            <div className="col-xs-6">
+              <button className="btn btn-success pull-right">
+                <span className="glyphicon glyphicon-plus-sign" /> Add
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
-
-export default App;
