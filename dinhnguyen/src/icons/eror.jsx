bf6653e73b6c3adebc823ca8d467323d1ae796be
nguyen1911 <143236655+nguyen1911@users.noreply.github.com>
2023-11-30 21:50:45 +0700

    kodj89i

--- dinhnguyen/src/icons/eror.jsx
+++
@@ -1,8 +0,0 @@
-import { IconConText} from "react-icons";
-import {FaExclamationTriangle} from "react-icons/fa";
-
-export default function ErrorIcon(){
-    return (
-        <IconConText.provider value={{color}}
-    )
-}
