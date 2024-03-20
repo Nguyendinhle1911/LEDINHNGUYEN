69223113c74399d52c56d11055569787a4461123
Nguyên ĐZ <143236234+Nguyendinhle1911@users.noreply.github.com>
2024-03-20 21:33:59 +0700

    Add files via upload

---
+++ MultAndDivide.java
@@ -0,0 +1,13 @@
+package academy;
+
+public class MultAndDivide {
+    public static void main(String[] args) {
+        double subtotal = 30;
+        double tax = 0.0875;
+        double total = subtotal + (subtotal * tax);
+        System.out.println("Total: " + total);
+        double perPerson = total / 4;
+        System.out.println("perPerson: " + perPerson);
+    }
+
+}
