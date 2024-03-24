09d0b6d329d1fb63e42539f988c67bd1db6eb9d7
Nguyên ĐZ <143236234+Nguyendinhle1911@users.noreply.github.com>
2024-03-24 22:44:39 +0700

    Add files via upload

---
+++ PhuQuocDog.java
@@ -0,0 +1,10 @@
+package session3;
+
+public class PhuQuocDog extends Dog{
+    public PhuQuocDog(String name, int age, int height) {
+        super(name, age, height);
+    }
+    public String getAnimalName(){
+        return "PhuQuocDog";
+    }
+}
