437d3156433aa57b58aac16f3906bda64c944b3a
Nguyên ĐZ <143236234+Nguyendinhle1911@users.noreply.github.com>
2024-03-26 08:33:21 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+package LAP04;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new SportCar();
+        Car c = new LuxuryCar();
     }
 }
