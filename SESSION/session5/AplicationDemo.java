498786bde9747d527996a2b29016e3818e3998f9
Nguyên dey <143236234+Nguyendinhle1911@users.noreply.github.com>
2024-04-05 22:56:54 +0700

    Add files via upload

---
+++ SESSION/session5/AplicationDemo.java
@@ -0,0 +1,16 @@
+package SESSION.session5;
+
+import java.util.Scanner;
+
+public class AplicationDemo {
+    public static void main(String[] args){
+        Scanner Scanner = new Scanner(System.in);
+    IntegerArrayDemo integerArrayDemo = new IntegerArrayDemo();
+    System.out.println("how many.....");
+        java.util.Scanner sc;
+        int size = sc.nextInt();
+    int[] intArray = new int[size];
+        IntegerArrayDemo.createArray(intArray);
+    integerArrayDemo.show(intArray);
+    System.out.println();}
+}
